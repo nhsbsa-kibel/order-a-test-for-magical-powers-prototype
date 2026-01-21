5253e018f4f49748ee110c2767d1d4dcad41c6ff
nhsbsa-kibel <kibel@nhsbsa.nhs.uk>
2026-01-21 15:11:35 +0000

    added branching from magical powers

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,31 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/magical-powers-answer', function (req, res) {
+  const data = req.session.data
+  const doYouHaveMagicalPowers = data.doYouHaveMagicalPowers
+
+  if (doYouHaveMagicalPowers === "Yes") {
+
+    res.redirect('/details')
+
+  } else if (doYouHaveMagicalPowers === "No") {
+
+    res.redirect('/ineligible')
+
+  } else if (doYouHaveMagicalPowers === "I'm not sure") {
+
+    res.redirect('/ineligible')
+
+  } else {
+
+    // No answer selected, return to question
+    res.redirect('/magical-powers')
+
+  }
+})
+
+
+
+
 module.exports = router
