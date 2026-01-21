aa970740f1ca8a86c51aa75412748970563338ed
nhsbsa-kibel <kibel@nhsbsa.nhs.uk>
2026-01-21 10:52:50 +0000

    Add service name

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'Order a test to check if you have magical powers',
 
   // Port to run nodemon on locally
   port: 2000,
